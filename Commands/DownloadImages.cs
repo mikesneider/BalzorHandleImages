4ce3acfc348441a4585957876e9d1e6b42bef5ea
Mike <mikesneider@users.noreply.github.com>
2022-07-22 16:51:49 -0500

    se decide mostrar la imagen directamente desde blob con la URL fija en lugar de irla a buscar nuevamente, en la BD se montaría la imagen con URL

--- Commands/DownloadImages.cs
+++ Commands/DownloadImages.cs
@@ -70,9 +70,19 @@ namespace BalzorHandleImages.Commands
                 Console.WriteLine(item.Name);
                 //Console.WriteLine(item.GetType());
             }
+            //public async Task<string> GetUrlImageAsync(string imageName)
+            //{
+            //    var blobContainer = await _blobFactory.GetContainer("uploaded-images", new CancellationToken());
+            //    var cloudFile = blobContainer.GetBlobClient(imageName);
+            //    Stream fileStream = new MemoryStream();
+            //    cloudFile.DownloadTo(fileStream);
+            //    //return fileStream;
+            //    //Puedo transformat filestream a Base64?
+            //    Console.WriteLine(cloudFile.Uri);
+            //    return cloudFile.Uri.ToString();
+            //}
 
 
-            
 
         }
 
